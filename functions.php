f9525a9b42c98440e01b91f38f3cf4cafa5582a4
Fredrik Nilsson <fn@live.se>
2015-11-29 08:00:51 +0000

    Cosmetic tabbing

--- functions.php
+++ functions.php
@@ -22,14 +22,11 @@ add_filter( 'the_author', 'guest_author_name' );
 add_filter( 'get_the_author_display_name', 'guest_author_name' );
 
 function guest_author_name( $name ) {
-global $post;
-
-$author = get_post_meta( $post->ID, 'guest-author', true );
-
-if ( $author )
-$name = $author;
-
-return $name;
+    global $post;
+    $author = get_post_meta( $post->ID, 'guest-author', true );
+    if ( $author )
+        $name = $author;
+    return $name;
 }
 
 /**
